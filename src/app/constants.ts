03eb44d8f00c03d21472dbc3e1e5b22cf0fe1478
pmoran2550 <patmoran@wi.rr.com>
2025-08-08 21:20:46 -0500

    Change to 2025

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -14,4 +14,4 @@ export const AUTH0_DOMAIN="dev-aucf7wnb.us.auth0.com";
 export const AUTH0_CLIENT_ID="PoJt9Tj9kShVTwtjM9GjzkGg1HNDWmuh";
 export const AUTH0_AUDIENCE="https://ffbdraft/api";
 export const AUTH0_FFBCLAIM='https://ffbview.com/roles';
-export const DRAFT_YEAR=2024;
+export const DRAFT_YEAR=2025;
